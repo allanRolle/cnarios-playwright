9da7f00e9e1fcf77eea78a9c969753a748e525b9
Allan Rolle <allan.rolle@laposte.net>
2026-03-30 16:13:43 +0200

    Button test cases completed

--- e2e/concepts/button.spec.ts
+++ e2e/concepts/button.spec.ts
@@ -1,16 +1,91 @@
 import { test, expect } from '../fixtures'
 
 test.describe('Button Component', () => {
-  test("Button text and icon should change to 'Following' with a check icon", async ({
-    buttonPage,
-  }) => {
-    await buttonPage.verifyStatus('Follow')
-    await Promise.all([
-      buttonPage.followButton.click(),
-      expect(buttonPage.followButton).toBeDisabled(),
-      await buttonPage.verifyStatus('Processing...'),
-    ])
-    await expect(buttonPage.followButton).toBeEnabled({ timeout: 10000 })
-    await buttonPage.verifyStatus('Following')
+  test.describe('Happy Paths', () => {
+    test("Button text and icon should change to 'Following' with a check icon", async ({
+      buttonPage,
+    }) => {
+      await expect(buttonPage.followButton).toBeVisible()
+      await expect(buttonPage.buttonIcon).toHaveClass(/add-person-icon/)
+      await expect(buttonPage.followButton).toHaveText('Follow')
+      await buttonPage.clickFollowButton()
+      await buttonPage.verifyStatus('Following')
+      await expect(buttonPage.buttonIcon).toHaveClass(/done-icon/)
+    })
+    test("Tooltip should display 'Click to follow' or 'Click to unfollow' based on state", async ({
+      buttonPage,
+    }) => {
+      await buttonPage.followButton.hover()
+      await expect(buttonPage.tooltip).toBeVisible()
+      await expect(buttonPage.tooltip).toHaveText('Follow')
+      await buttonPage.clickFollowButton()
+      await buttonPage.followButton.hover()
+      await expect(buttonPage.tooltip).toHaveText('Unfollow')
+    })
+    test("'Processing...' should appear before state changes and the button should be disabled during this time", async ({
+      buttonPage,
+    }) => {
+      await buttonPage.clickFollowButton()
+      await Promise.all([
+        expect(buttonPage.followButton).toBeDisabled(),
+        buttonPage.verifyStatus('Processing...'),
+      ])
+      await expect(buttonPage.followButton).toBeEnabled({ timeout: 10000 })
+      await buttonPage.verifyStatus('Following')
+    })
+    test("Button should return to Follow state after clicking 'Following'", async ({
+      buttonPage,
+    }) => {
+      await buttonPage.clickFollowButton()
+      await buttonPage.verifyStatus('Following')
+      await buttonPage.clickFollowButton()
+      await buttonPage.verifyStatus('Follow')
+    })
+    test('The selected suggestion card should be removed from the visible list', async ({
+      buttonPage,
+    }) => {
+      const initialCards = await buttonPage.page.locator('.css-12nllm1').count()
+      await buttonPage.clickRemoveButton()
+      await expect(buttonPage.page.locator('.css-12nllm1')).toHaveCount(
+        initialCards - 1
+      )
+    })
+  })
+  test.describe('Negative paths and edge cases', () => {
+    test('Rapidly clicking the Follow button should not cause inconsistent states', async ({
+      buttonPage,
+    }) => {
+      await buttonPage.clickFollowButton()
+      await buttonPage.clickFollowButton()
+      await buttonPage.verifyStatus('Processing...')
+      await expect(buttonPage.followButton).toBeDisabled()
+      await expect(buttonPage.followButton).toBeEnabled({ timeout: 10000 })
+      const status = await buttonPage.followButton.textContent()
+      expect(['Follow', 'Following']).toContain(status)
+    })
+    test('Unable to follow if button is desabled', async ({ buttonPage }) => {
+      await expect(buttonPage.disabledButton).toBeDisabled()
+      await buttonPage.disabledButton.click({ force: true })
+      await expect(buttonPage.disabledButton).toBeDisabled()
+    })
+    test('Ignore clicks near button (missed clicks) should not trigger state changes', async ({
+      buttonPage,
+    }) => {
+      const initialStatus = await buttonPage.followButton.textContent()
+      await buttonPage.page.mouse.click(
+        (await buttonPage.followButton.boundingBox())!.x - 10,
+        (await buttonPage.followButton.boundingBox())!.y + 10
+      )
+      await buttonPage.verifyStatus(initialStatus as 'Follow' | 'Following')
+    })
+    test('Button remains functional when visually hidden', async ({
+      buttonPage,
+    }) => {
+      await buttonPage.followButton.evaluate((button) => {
+        button.style.opacity = '0'
+      })
+      await buttonPage.clickFollowButton()
+      await buttonPage.verifyStatus('Following')
+    })
   })
 })
--- e2e/pages/ButtonPage.ts
+++ e2e/pages/ButtonPage.ts
@@ -3,14 +3,29 @@ import { BasePage } from './BasePage'
 
 export class ButtonPage extends BasePage {
   readonly followButton: Locator
+  readonly removeButton: Locator
+  readonly disabledButton: Locator
+  readonly buttonIcon: Locator
+  readonly tooltip: Locator
 
   constructor(page: Page) {
     super(page)
     this.followButton = page.locator('.css-15q2cw4').nth(1).locator('button')
+    this.disabledButton = page.locator('.css-15q2cw4').nth(0).locator('button')
+    this.removeButton = page
+      .locator('.css-12nllm1')
+      .nth(1)
+      .locator('.css-xz9haa')
+    this.buttonIcon = this.followButton.locator('svg')
+    this.tooltip = page.locator('.css-1ydfzmo')
   }
 
   async clickFollowButton() {
     await this.followButton.click()
+  }
+
+  async clickRemoveButton() {
+    await this.removeButton.click()
   }
 
   async verifyStatus(status: 'Follow' | 'Processing...' | 'Following') {
